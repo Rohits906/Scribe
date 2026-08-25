cec93da4018657dae487edefc3f3c617c3961ada
Rohit Soni <rohitsoni7038@gmail.com>
2026-08-25 20:42:06 +0530

    Removed un-used files

--- backend/src/middleware/notFound.js
+++
@@ -1,5 +0,0 @@
-import { HttpError } from '../utils/httpError.js';
-
-export default function notFound(req, res, next) {
-  next(new HttpError(404, `Route not found: ${req.method} ${req.originalUrl}`));
-}
--- frontend/src/types/note.ts
+++
@@ -1,9 +0,0 @@
-export interface Note {
-  id: string;
-  title: string;
-  body: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-export type NoteDraft = Pick<Note, 'title' | 'body'>;
